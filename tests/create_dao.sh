08295592309ef68550da2eb9dc5f5a179be70feb
anish-singh-rawat <www.computernetwork@gmail.com>
2024-09-24 17:45:28 +0530

    fix dao_canister : implement approve / reject and expired vote status

--- tests/create_dao.sh
+++ tests/create_dao.sh
@@ -20,7 +20,7 @@ for ((i = 1; i <= COUNT; i++)); do
       token_supply = 1000;
       token_symbol = "GOKU";
       members = vec{
-        principal "m2zqz-pr5r2-ozayk-w5trf-mt6mw-7vuys-mitrw-4qdpb-dm5p7-77ey6-fae";
+        principal "xtbxv-f7q6j-ophog-cryki-6oaez-j7agl-i74rt-wnpbm-bywl2-62j32-vqe";
         principal "rmehg-adw5r-6trpq-epk4r-tyl4c-dd2u4-erbw4-kcjzr-rrjpf-dfvi2-oae";
         principal "yxtej-lmfuu-rp3yv-xzu2h-6q43c-7iast-yiwff-z552q-6ugas-pyd6b-fae";
       };
@@ -29,7 +29,7 @@ for ((i = 1; i <= COUNT; i++)); do
       linksandsocials = vec{
         "https://twitter.com/sampledao";
       };
-    required_votes = 100;
+    required_votes = 3;
       image_id = "1";
       image_content = vec {10};
       image_title = "sample.jpg";
@@ -41,7 +41,7 @@ for ((i = 1; i <= COUNT; i++)); do
         record {
             group_name = "Example Group";
             group_members = vec {
-            principal "m2zqz-pr5r2-ozayk-w5trf-mt6mw-7vuys-mitrw-4qdpb-dm5p7-77ey6-fae";
+            principal "xtbxv-f7q6j-ophog-cryki-6oaez-j7agl-i74rt-wnpbm-bywl2-62j32-vqe";
             principal "rmehg-adw5r-6trpq-epk4r-tyl4c-dd2u4-erbw4-kcjzr-rrjpf-dfvi2-oae";
             principal "yxtej-lmfuu-rp3yv-xzu2h-6q43c-7iast-yiwff-z552q-6ugas-pyd6b-fae";  
            };
@@ -51,7 +51,7 @@ for ((i = 1; i <= COUNT; i++)); do
         record {
             group_name = "Example Group2";
             group_members = vec {
-            principal "m2zqz-pr5r2-ozayk-w5trf-mt6mw-7vuys-mitrw-4qdpb-dm5p7-77ey6-fae";
+            principal "xtbxv-f7q6j-ophog-cryki-6oaez-j7agl-i74rt-wnpbm-bywl2-62j32-vqe";
             principal "rmehg-adw5r-6trpq-epk4r-tyl4c-dd2u4-erbw4-kcjzr-rrjpf-dfvi2-oae";
             principal "yxtej-lmfuu-rp3yv-xzu2h-6q43c-7iast-yiwff-z552q-6ugas-pyd6b-fae";  
            };
