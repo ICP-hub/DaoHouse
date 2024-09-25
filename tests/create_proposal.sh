f151c0c9494b5d0d77fe7d61029bad55a1a3c867
anish-singh-rawat <www.computernetwork@gmail.com>
2024-09-25 18:03:13 +0530

    fix dao_canister : imple auto run functions using set interval

--- tests/create_proposal.sh
+++ tests/create_proposal.sh
@@ -2,7 +2,7 @@ test10=$(dfx canister call b77ix-eeaaa-aaaaa-qaada-cai proposal_to_add_member_to
   record {
     group_name = "Example Group";
     new_member = principal "ui2dz-muwb7-dhwzj-545cc-ets5d-wc3gr-hi5qi-zavzz-arybg-fsh6r-eae";
-    description = "we want add an member to group";
+    description = "we want add an member to ddddddd";
   }
 )')
  echo "$test10"
