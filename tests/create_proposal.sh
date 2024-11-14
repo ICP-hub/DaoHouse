2398d212e2ad1ab7ba96ef08049753dcf83a9f1f
anishsinghQB <anish.singh@quadbtech.com>
2024-11-14 10:38:18 +0530

    feat frontnend : correct code for pagigation

--- tests/create_proposal.sh
+++ tests/create_proposal.sh
@@ -1,9 +1,14 @@
+COUNT=1000
 
-test10=$(dfx canister call b77ix-eeaaa-aaaaa-qaada-cai proposal_to_remove_member_to_group '(
+for ((i = 1; i <= COUNT; i++)); do 
+test10=$(dfx canister call b77ix-eeaaa-aaaaa-qaada-cai proposal_to_remove_member_to_dao '(
   record {
     group_name = "Example Group";
-    action_member = principal "aaaaa-aa";
+    action_member = principal "eylnw-gnl2t-nit3s-jnkej-425fc-xmlz2-5xjdc-xbkjb-x5fsp-gn3eb-gae";
     description = "we want add a member to Example Group";
+    proposal_entry = "Council";
   }
 )')
  echo "$test10"
+
+done
