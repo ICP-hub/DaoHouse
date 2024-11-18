e83ec719c666fd1d817fac38cdaf6e0db66a538d
anishsinghQB <anish.singh@quadbtech.com>
2024-11-18 13:36:02 +0530

    fix frontend : correct code for Bounty raised , token transfer, Bounty done input default fields

--- src/daohouse_frontend/src/pages/Proposals/BountyDone.jsx
+++ src/daohouse_frontend/src/pages/Proposals/BountyDone.jsx
@@ -36,7 +36,7 @@ const BountyDone = ({ bountyDone, handleInputBountyDone, dao }) => (
           id="bountyTokens"
           type="number"
           name="tokens"
-          value={dao.total_tokens ? dao.total_tokens : bountyDone.tokens}
+          value={bountyDone.tokens}
           onChange={handleInputBountyDone}
           className="w-full px-4 py-3 border-opacity-30 border border-[#aba9a5] rounded-xl bg-transparent"
           placeholder="Enter Tokens"
@@ -44,31 +44,6 @@ const BountyDone = ({ bountyDone, handleInputBountyDone, dao }) => (
           min={1}
         />
       </div>
-    {/* <div className="mb-4">
-      <label htmlFor="actionMember" className="block mb-2 font-semibold text-xl">Action Member (Principal)</label>
-      <input
-        id="action_member"
-        type="text"
-        name="action_member"
-        value={bountyDone.action_member}
-        onChange={handleInputBountyDone}
-        className="w-full px-4 py-3 border-opacity-30 border border-[#aba9a5] rounded-xl bg-transparent"
-        placeholder="Enter action member principal"
-      />
-    </div> */}
-    {/* <div className="mb-4">
-      <label htmlFor="Bounty Task" className="block mb-2 font-semibold text-xl">Bounty Task</label>
-      <input
-        id="bounty_task"
-        type="text"
-        name="bounty_task"
-        value={bountyDone.bounty_task}
-        onChange={handleInputBountyDone}
-        className="w-full px-4 py-3 border-opacity-30 border border-[#aba9a5] rounded-xl bg-transparent"
-        required
-      />
-    </div> */}
-
   </>
 );
 
