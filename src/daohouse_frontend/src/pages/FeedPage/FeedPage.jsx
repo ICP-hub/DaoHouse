f529245a3914c7e292049f780eb8a2d41d59d4eb
AmolD0105 <amol.dhawle@quadbtech.com>
2024-11-28 12:29:15 +0530

    minor ui change

--- src/daohouse_frontend/src/pages/FeedPage/FeedPage.jsx
+++ src/daohouse_frontend/src/pages/FeedPage/FeedPage.jsx
@@ -12,7 +12,8 @@ import Pagination from "../../Components/pagination/Pagination";
 
 const FeedPage = () => {
   const [showLoginModal, setShowLoginModal] = useState(false);
-  const { isAuthenticated, login, signInNFID, backendActor, createDaoActor } = useAuth();
+  const { isAuthenticated, login, signInNFID, backendActor, createDaoActor } =
+    useAuth();
   const navigate = useNavigate();
   const [sortOrder, setSortOrder] = useState("newest");
   const [searchTerm, setSearchTerm] = useState("");
@@ -24,14 +25,13 @@ const FeedPage = () => {
   let itemsPerPage = 4;
   const className = "FeedPage";
 
-
   const handleLogin = async () => {
     setLoading(true);
     try {
       await login("Icp");
       window.location.reload();
     } catch (error) {
-      console.error('Login failed:', error);
+      console.error("Login failed:", error);
     } finally {
       setLoading(false);
     }
@@ -43,7 +43,7 @@ const FeedPage = () => {
       await signInNFID();
       window.location.reload();
     } catch (error) {
-      console.error('NFID login failed:', error);
+      console.error("NFID login failed:", error);
     } finally {
       setLoading(false);
     }
@@ -54,7 +54,6 @@ const FeedPage = () => {
   };
 
   const fetchAllProposals = async (pagination = {}) => {
-    
     try {
       setLoading(true);
       const allDaos = await backendActor.get_all_dao();
@@ -65,7 +64,9 @@ const FeedPage = () => {
         };
         try {
           const daoActor = await createDaoActor(dao.dao_canister_id);
-          const daoProposals = await daoActor.get_all_proposals(proposalPagination);
+          const daoProposals = await daoActor.get_all_proposals(
+            proposalPagination
+          );
           const proposalsWithDaoId = daoProposals.map((proposal) => ({
             ...proposal,
             dao_canister_id: dao.dao_canister_id,
@@ -75,10 +76,12 @@ const FeedPage = () => {
           const proposalsToDisplay = proposalsWithDaoId.slice(0, itemsPerPage);
           setProposals(proposalsToDisplay);
         } catch (error) {
-          console.error(`Error fetching proposals from DAO ${dao.dao_canister_id}:`, error);
+          console.error(
+            `Error fetching proposals from DAO ${dao.dao_canister_id}:`,
+            error
+          );
         }
       }
-
     } catch (error) {
       console.error("Error fetching DAOs and proposals:", error);
     } finally {
@@ -86,17 +89,17 @@ const FeedPage = () => {
     }
   };
 
-
   useEffect(() => {
     if (!isAuthenticated) {
       setShowLoginModal(true);
       return;
     }
     setShowLoginModal(false);
-    fetchAllProposals({ start: (currentPage - 1) * itemsPerPage, end: currentPage * itemsPerPage });
-
+    fetchAllProposals({
+      start: (currentPage - 1) * itemsPerPage,
+      end: currentPage * itemsPerPage,
+    });
   }, [isAuthenticated, backendActor, createDaoActor, currentPage, searchTerm]);
-
 
   const handleModalClose = () => {
     setShowLoginModal(false);
@@ -119,7 +122,9 @@ const FeedPage = () => {
         <Container
           classes={`__filter w-100 desktop:h-[220px] h-[168px] top-[70px] big_phone:p-20 small_phone:p-10 py-4 flex flex-col items-start justify-center ${className}`}
         >
-          <h1 className="mobile:text-5xl text-3xl p-4 small_phone:mx-[-24px] big_phone:mx-[-60px] lg:mx-2 text-white">Social Feed</h1>
+          <h1 className="mobile:text-5xl text-3xl p-4 small_phone:mx-[-24px] big_phone:mx-[-60px] lg:mx-2 text-white">
+            Social Feed
+          </h1>
         </Container>
       </div>
 
@@ -146,76 +151,81 @@ const FeedPage = () => {
             />
           </div>
           <button
-                className="bg-white text-[#05212C] flex items-center justify-between gap-3 lg:px-6 md:px-5 shadow-xl lg:py-3 py-2 px-4 rounded-full shadow-md hover:bg-[#f0f4f7] transition-all duration-300 relative"
-                onClick={() => setShowFilter((prev) => !prev)}
+            className="bg-white text-[#05212C] flex items-center justify-between gap-3 lg:px-6 md:px-5 shadow-xl lg:py-3 py-2 px-4 rounded-full shadow-md hover:bg-[#f0f4f7] transition-all duration-300 relative"
+            onClick={() => setShowFilter((prev) => !prev)}
+          >
+            <div className="flex items-center">
+              <svg
+                width="24"
+                height="24"
+                viewBox="0 0 27 27"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
               >
-                <div className="flex items-center">
-                  <svg
-                    width="24"
-                    height="24"
-                    viewBox="0 0 27 27"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M10.1251 17.55H16.8751C17.0437 17.5497 17.2064 17.6125 17.3311 17.7262C17.4557 17.8398 17.5333 17.996 17.5486 18.164C17.5638 18.332 17.5156 18.4996 17.4134 18.6338C17.3113 18.768 17.1626 18.8591 16.9966 18.8892L16.8751 18.9H10.1251C9.9564 18.9003 9.79372 18.8375 9.66907 18.7238C9.54442 18.6102 9.46683 18.454 9.45159 18.286C9.43635 18.118 9.48456 17.9504 9.58672 17.8162C9.68888 17.682 9.83759 17.5909 10.0036 17.5608L10.1251 17.55ZM7.42507 12.15H19.5751C19.7438 12.1497 19.9064 12.2125 20.0311 12.3262C20.1557 12.4398 20.2333 12.596 20.2486 12.764C20.2638 12.932 20.2156 13.0996 20.1134 13.2338C20.0113 13.368 19.8626 13.4591 19.6966 13.4892L19.5751 13.5H7.42507C7.2564 13.5003 7.09371 13.4375 6.96907 13.3238C6.84442 13.2102 6.76683 13.054 6.75159 12.886C6.73635 12.718 6.78456 12.5504 6.88672 12.4162C6.98888 12.282 7.13759 12.1909 7.30357 12.1608L7.42507 12.15ZM4.72507 6.75H22.2751C22.4438 6.74969 22.6064 6.81255 22.7311 6.92619C22.8557 7.03983 22.9333 7.19602 22.9486 7.36401C22.9638 7.532 22.9156 7.6996 22.8134 7.83382C22.7113 7.96804 22.5626 8.05915 22.3966 8.0892L22.2751 8.1H4.72507C4.5564 8.10031 4.39371 8.03746 4.26907 7.92381C4.14442 7.81017 4.06683 7.65398 4.05159 7.48599C4.03635 7.318 4.08456 7.1504 4.18672 7.01618C4.28888 6.88196 4.43759 6.79085 4.60357 6.7608L4.72507 6.75Z"
-                      fill="black"
-                    />
-                  </svg>
-                </div>
-                <span className="text-[16px] ml-2 hidden md:flex">Filter</span>
+                <path
+                  d="M10.1251 17.55H16.8751C17.0437 17.5497 17.2064 17.6125 17.3311 17.7262C17.4557 17.8398 17.5333 17.996 17.5486 18.164C17.5638 18.332 17.5156 18.4996 17.4134 18.6338C17.3113 18.768 17.1626 18.8591 16.9966 18.8892L16.8751 18.9H10.1251C9.9564 18.9003 9.79372 18.8375 9.66907 18.7238C9.54442 18.6102 9.46683 18.454 9.45159 18.286C9.43635 18.118 9.48456 17.9504 9.58672 17.8162C9.68888 17.682 9.83759 17.5909 10.0036 17.5608L10.1251 17.55ZM7.42507 12.15H19.5751C19.7438 12.1497 19.9064 12.2125 20.0311 12.3262C20.1557 12.4398 20.2333 12.596 20.2486 12.764C20.2638 12.932 20.2156 13.0996 20.1134 13.2338C20.0113 13.368 19.8626 13.4591 19.6966 13.4892L19.5751 13.5H7.42507C7.2564 13.5003 7.09371 13.4375 6.96907 13.3238C6.84442 13.2102 6.76683 13.054 6.75159 12.886C6.73635 12.718 6.78456 12.5504 6.88672 12.4162C6.98888 12.282 7.13759 12.1909 7.30357 12.1608L7.42507 12.15ZM4.72507 6.75H22.2751C22.4438 6.74969 22.6064 6.81255 22.7311 6.92619C22.8557 7.03983 22.9333 7.19602 22.9486 7.36401C22.9638 7.532 22.9156 7.6996 22.8134 7.83382C22.7113 7.96804 22.5626 8.05915 22.3966 8.0892L22.2751 8.1H4.72507C4.5564 8.10031 4.39371 8.03746 4.26907 7.92381C4.14442 7.81017 4.06683 7.65398 4.05159 7.48599C4.03635 7.318 4.08456 7.1504 4.18672 7.01618C4.28888 6.88196 4.43759 6.79085 4.60357 6.7608L4.72507 6.75Z"
+                  fill="black"
+                />
+              </svg>
+            </div>
+            <span className="text-[16px] ml-2 hidden mobile:flex">Filter</span>
 
-                {showFilter && (
-                  <div className="absolute bg-white border border-[#AAC8D6] rounded-lg shadow-lg mt-2 w-32 z-10 top-full md:left-0 left-[-48px]">
-                    <div
-                      className="cursor-pointer p-2 text-sm hover:bg-[#f0f4f7] rounded-t-lg"
-                      onClick={() => setSortOrder('newest')}
-                    >
-                      Newest to Oldest
-                    </div>
-                    <div
-                      className="cursor-pointer p-2 text-sm hover:bg-[#f0f4f7] rounded-b-lg"
-                      onClick={() => setSortOrder('oldest')}
-                    >
-                      Oldest to Newest
-                    </div>
-                  </div>
-                )}
-              </button>
+            {showFilter && (
+              <div className="absolute bg-white border border-[#AAC8D6] rounded-lg shadow-lg mt-2 w-32 z-10 top-full md:left-0 left-[-48px]">
+                <div
+                  className="cursor-pointer p-2 text-sm hover:bg-[#f0f4f7] rounded-t-lg"
+                  onClick={() => setSortOrder("newest")}
+                >
+                  Newest to Oldest
+                </div>
+                <div
+                  className="cursor-pointer p-2 text-sm hover:bg-[#f0f4f7] rounded-b-lg"
+                  onClick={() => setSortOrder("oldest")}
+                >
+                  Oldest to Newest
+                </div>
+              </div>
+            )}
+          </button>
         </Container>
       </div>
 
       {/* Post Section */}
-      <div className={`${className}__postCards mobile:px-10 px-6 pb-10 bg-[#c8ced3] gap-8 flex flex-col`}>
-    {loading ? (
-      <ProposalLoaderSkeleton />
-    ) : proposals.length === 0 ? (
-      <div className="flex justify-center items-center h-full mb-10 mt-10">
-        <Container className="w-full flex flex-col items-center justify-center">
-          <img src={nodata} alt="No Data" className="mb-1 ml-[42px]" />
-          <p className="text-center mt-4 text-gray-700 text-base">
-            You have not created any DAO
-          </p>
-        </Container>
-      </div>
-    ) : (
-      <div className="mx-2 small_phone:mx-4 mobile:mx-1 lg:mx-16 desktop:mx-20">
-        <Container>
-          <div className="desktop:mx-12">
-          <ProposalsContent
-            proposals={proposals}
-            isMember={true}
-            showActions={false}
-            sortOrder={sortOrder}
-            setSortOrder={setSortOrder}
-          />
-
+      <div
+        className={`${className}__postCards mobile:px-10 px-6 pb-10 bg-[#c8ced3] gap-8 flex flex-col`}
+      >
+        {loading ? (
+          <ProposalLoaderSkeleton />
+        ) : proposals.length === 0 ? (
+          <div className="flex justify-center items-center h-full mb-10 mt-10">
+            <Container className="w-full flex flex-col items-center justify-center">
+              <img src={nodata} alt="No Data" className="mb-1 ml-[42px]" />
+              <p className="text-center mt-4 text-gray-700 text-base">
+                You have not created any DAO
+              </p>
+            </Container>
           </div>
-          <Pagination currentPage={currentPage} setCurrentPage={setCurrentPage} hasMore={hasMore} />
-        </Container>
+        ) : (
+          <div className="mx-2 small_phone:mx-4 mobile:mx-1 lg:mx-16 desktop:mx-20">
+            <Container>
+              <div className="desktop:mx-12">
+                <ProposalsContent
+                  proposals={proposals}
+                  isMember={true}
+                  showActions={false}
+                  sortOrder={sortOrder}
+                  setSortOrder={setSortOrder}
+                />
+              </div>
+              <Pagination
+                currentPage={currentPage}
+                setCurrentPage={setCurrentPage}
+                hasMore={hasMore}
+              />
+            </Container>
+          </div>
+        )}
       </div>
-    )}
-  </div>
 
       {/* Login Modal */}
       {showLoginModal && (
@@ -229,6 +239,5 @@ const FeedPage = () => {
     </div>
   );
 };
-
 
 export default FeedPage;
