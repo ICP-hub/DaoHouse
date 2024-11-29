9fe847d39892d243ed7426814d96f2a18de3dbe4
AmolD0105 <amol.dhawle@quadbtech.com>
2024-11-29 11:03:46 +0530

    Add loader in vote options, fix joined button's position in dao profile

--- src/daohouse_frontend/src/Components/Proposals/Card.jsx
+++ src/daohouse_frontend/src/Components/Proposals/Card.jsx
@@ -293,6 +293,8 @@ export default function Card({
   const handleVoteSubmit = async (voteStatus) => {
     if (!voteStatus) return;
 
+    setVoteStatus(voteStatus)
+
     try {
       setIsVoteLoading(true);
 
@@ -1054,7 +1056,7 @@ export default function Card({
                             onClick={() => handleVoteSubmit("In Favor")}
                           >
                             <div
-                              className={`relative rounded-lg h-10 flex items-center ${
+                              className={`relative rounded-lg h-10 flex items-center bg-gray-100 ${
                                 isVoteLoading && voteStatus === "In Favor"
                                   ? "sliding-lines"
                                   : ""
@@ -1079,7 +1081,7 @@ export default function Card({
                             onClick={() => handleVoteSubmit("Against")}
                           >
                             <div
-                              className={`relative rounded-lg h-10 flex items-center ${
+                              className={`relative rounded-lg h-10 flex items-center bg-gray-100 ${
                                 isVoteLoading && voteStatus === "Against"
                                   ? "sliding-lines"
                                   : ""
