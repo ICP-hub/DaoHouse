3f6ccb18210db957d1b972f27112a086998bf5a8
AmolD0105 <amol.dhawle@quadbtech.com>
2024-12-03 12:01:25 +0530

    Add protected routes

--- src/daohouse_frontend/src/App.jsx
+++ src/daohouse_frontend/src/App.jsx
@@ -1,5 +1,5 @@
-import React, { Children } from "react";
-import './global.scss'
+import React from "react";
+import './global.scss';
 import PostProvider from "./PostProvider";
 import Dao from "./pages/dao/Dao";
 import Dashboard from "../src/pages/Home/Dashboard";
@@ -14,7 +14,7 @@ import EditProfile from "./pages/EditProfile/EditProfile";
 import CreateProposal from "./pages/Proposals/CreateProposal";
 import MyPosts from "./Components/MyProfile/MyPosts/MyPosts";
 import AboutMe from "./Components/MyProfile/AboutMe/AboutMe";
-import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import DaoProfile from "./pages/DaoProfile/DaoProfile";
 import "react-toastify/dist/ReactToastify.css";
 import "react-quill/dist/quill.snow.css";
@@ -25,51 +25,102 @@ import Following from "./Components/MyProfile/Following/Following";
 import { Toaster } from 'react-hot-toast';
 import ScrollToTop from "./Components/utils/ScrollToTop";
 import ProposalsDetails from "./pages/FeedPage/ProposalDetails";
-
+import ProtectedRoute from "./utils/ProtectedRoute";
 
 const App = () => {
-
-  const location = useLocation();
-
   return (
     <PostProvider>
-        <Navbar />
-       
-        <Toaster
-  position="top-center"
-  reverseOrder={false}
-/>
-        <ScrollToTop />
-        <Routes>
-          <Route path="/*" element={<Error404 />} />
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/social-feed" element={<FeedPage />} />
-          <Route path="/social-feed/proposal/:proposalId/dao/:daoCanisterId" element={<ProposalsDetails />} />
-          {/* <Route path="/proposals" element={<Proposals />} /> */}
-          <Route path="/create-proposal/:daoCanisterId" element={<CreateProposal />} />
-          <Route path="/dao" element={<Dao />} />
-          <Route path="/dao/create-dao" element={<CreateDao />} />
-          <Route path="/dao/profile/:daoCanisterId" element={<DaoProfile />} />
-          <Route path="/edit-profile" element={<EditProfile />} />
-          <Route path="/post/:postID" element={<Post />} />
-          <Route
-            path="/my-profile"
-            element={<MyProfile childComponent={<AboutMe />} />}
-          />
-          <Route path="/my-proposals" element={<MyProposals />} />
-          <Route path="/my-proposals/:id" element={<SingleProposal />} />
-          <Route
-            path="/my-profile/posts"
-            element={<MyProfile childComponent={<MyPosts />} />}
-          />
-             <Route
-            path="/my-profile/following"
-            element={<MyProfile childComponent={<Following />} />}
-          />
-        </Routes>
-        <Footer />
- 
-      {/* <ToastContainer /> */}
+      <Navbar />
+      <Toaster position="top-center" reverseOrder={false} />
+      <ScrollToTop />
+      <Routes>
+        <Route path="/*" element={<Error404 />} />
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/social-feed" element={<FeedPage />} />
+        <Route path="/social-feed/proposal/:proposalId/dao/:daoCanisterId" element={<ProposalsDetails />} />
+        <Route
+          path="/create-proposal/:daoCanisterId"
+          element={
+            <ProtectedRoute>
+              <CreateProposal />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/dao" element={<Dao />} />
+        <Route
+          path="/dao/create-dao"
+          element={
+            <ProtectedRoute>
+              <CreateDao />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/dao/profile/:daoCanisterId"
+          element={
+            <ProtectedRoute>
+              <DaoProfile />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/edit-profile"
+          element={
+            <ProtectedRoute>
+              <EditProfile />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/post/:postID"
+          element={
+            <ProtectedRoute>
+              <Post />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/my-profile"
+          element={
+            <ProtectedRoute>
+              <MyProfile childComponent={<AboutMe />} />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/my-proposals"
+          element={
+            <ProtectedRoute>
+              <MyProposals />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/my-proposals/:id"
+          element={
+            <ProtectedRoute>
+              <SingleProposal />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/my-profile/posts"
+          element={
+            <ProtectedRoute>
+              <MyProfile childComponent={<MyPosts />} />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/my-profile/following"
+          element={
+            <ProtectedRoute>
+              <MyProfile childComponent={<Following />} />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+      <Footer />
     </PostProvider>
   );
 };
